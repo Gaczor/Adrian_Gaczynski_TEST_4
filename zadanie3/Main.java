dcf0d80c134c0be5a510cea6d146147e1ebc7cdc
Adrian Gaczyński <gaczor@hotmail.com>
2024-02-23 11:46:52 +0100

    TEST4

--- zadanie3/Main.java
+++ zadanie3/Main.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-        MinMaxService.getMinAndMax(numbers).ifPresent(minMax -> System.out.println("Min:" + minMax.getMin() + ", max: " + minMax.getMax()));
+        List<Integer> numbers = Arrays.asList(1, 2, 3, null, 5);
+        List<String> words = Arrays.asList("zoo", null, "apple", "banana");
 
-        List<String> strings = Arrays.asList("apple", "zoo", "cherry");
-        MinMaxService.getMinAndMax(strings).ifPresent(minMax -> System.out.println("Min:" + minMax.getMin() + ", max: " + minMax.getMax()));
-
+        try {
+            System.out.println("Min: " + MinMaxService.getMinAndMax(numbers).getMin() + ", Max: " + MinMaxService.getMinAndMax(numbers).getMax());
+            System.out.println("Min: " + MinMaxService.getMinAndMax(words).getMin() + ", Max: " + MinMaxService.getMinAndMax(words).getMax());
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
+        }
 
     }
 }
--- zadanie3/MinMaxService.java
+++ zadanie3/MinMaxService.java
@@ -2,16 +2,16 @@ package zadanie3;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
 
 public class MinMaxService {
-    public static <T extends Comparable<T>> Optional<MinMax<T>> getMinAndMax(List<T> elements) {
-        if (elements == null || elements.isEmpty()) {
-            return Optional.empty();
+    public static <T extends Comparable<T>> MinMax<T> getMinAndMax(List<T> elements) {
+        if (elements == null || elements.stream().filter(Objects::nonNull).toList().isEmpty()) {
+            throw new IllegalArgumentException("Lista nie może być pusta lub zawierać same nulle");
         }
-        T min = Collections.min(elements);
-        T max = Collections.max(elements);
+        T min = Collections.min(elements.stream().filter(Objects::nonNull).toList());
+        T max = Collections.max(elements.stream().filter(Objects::nonNull).toList());
 
-        return Optional.of(new MinMax<>(min, max));
+        return new MinMax<>(min, max);
     }
 }
